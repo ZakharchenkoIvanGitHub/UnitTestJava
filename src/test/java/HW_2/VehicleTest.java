50670e1de58b741eefafeb788013a600b58e54fe
Inav Zakharchenko <zakharka2000@ya.ru>
2023-09-20 23:28:11 +0300

    старт

---
+++ src/test/java/HW_2/VehicleTest.java
@@ -0,0 +1,13 @@
+package HW_2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+    @Test
+    public void testCarIsInstanceOfVehicle() {
+        Car car = new Car("Dodge", "Ram", 2010);
+        assertTrue(car instanceof Vehicle);
+    }
+}
